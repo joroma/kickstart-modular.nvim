669e6040108d54cfe81f67e8da75c2b09b9b1e17
joroma <johnroger.maglalang@gmail.com>
2025-03-02 11:16:00 +0800

    misc configs

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -71,6 +71,6 @@ vim.cmd 'set softtabstop=2'
 vim.cmd 'set shiftwidth=2'
 
 -- Set color scheme
-vim.cmd [[colorscheme everforest]]
+vim.cmd [[colorscheme tokyonight]]
 
 -- vim: ts=2 sts=2 sw=2 et
